990238442e09cb3f89cad06facc8a6b0043ba61c
vitorsoaresf <vitorsoaresf@gmail.com>
2021-09-27 21:48:35 -0300

    altera prototipos

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const n_discos = document.getElementById('n_discos');
-const min_movimentos = document.getElementById('min_movimentos').value = `${(2 ** n_discos.value) - 1}`;
+let min_movimentos = document.getElementById('min_movimentos').value = `${(2 ** n_discos.value) - 1}`;
+
+const torre1 = document.getElementById('torre1');
+const torre2 = document.getElementById('torre2');
+const torre3 = document.getElementById('torre3');
+
 
 n_discos.addEventListener('click', () => {
     /** SE NO MEIO DO JOGO ELE ESCOLHER OUTRO NUMERO
@@ -12,16 +17,23 @@ n_discos.addEventListener('click', () => {
 });
 
 // FUNCAO QUE IRA EXECUTAR NOSSA APLICACAO
-const app = () => {}
+function app(){
+}
 
 // FUNCAO QUE ADICIONA UMA QUANTIDADE INFORMANDA DE DISCOS
 // NA POSICAO ORIGINAL
-const addDiskParent = (numberDisks, parent) => {}
+function addDiskParent(element, parent){}
 
 // FUNCAO QUE DISPONIBILIZA OS DISCOS NA POSICAO INICIAL
 // PARA INICIO DO JOGO
-const newGame = () => {}
+function newGame(){
+    // criar o elemento
+    // estilizar o elemento
+    // apendar o filho
+}
 
 // FUNCAO QUE REMOVERA UM ELEMENTO FILHO DE UM PARENTE
 // ESSA FIUNCAO FUNCIONARA EM CONJUNTO COM A addDiskParent()
-const removeDiskParent = (parent) => {}
+function removeDiskParent(parent){}
+
+console.log(typeof parseInt('2'))
